d8d189b07f65db4982660408a4a1b7c0cc8a7b7d
Isao Yagi <isao@yahoo-inc.com>
2013-01-15 18:21:39 -0800

    add ability to inject a typer function; docblocks

--- index.js
+++ index.js
@@ -5,33 +5,54 @@ var fs = require('fs'),
     Stream = require('stream');
 
 
-function typer(err, stat) {
-    var type = "other";
+/**
+ * @param {object} err fs.stat() Error object, or null
+ * @param {object} stat fs.Stats object http://nodejs.org/docs/latest/api/fs.html#fs_class_fs_stats
+ * @param {string} item Pathname
+ * @return {string} Type of filesystem item and name of event emitted
+ */
+function typer(err, stat, item) {
+    var type = 'other';
     if (err) {
-        type = "error";
+        type = 'error';
     } else if (stat.isFile()) {
-        type = "file";
+        type = 'file';
     } else if (stat.isDirectory()) {
-        type = "dir";
+        type = 'dir';
     }
     return type;
 }
 
+/**
+ * @param {string} item File system path.
+ * @param {array} list Items remaining to fs.stat().
+ * @param {object} self This instance object.
+ */
 function getStatCb(item, list, self) {
     return function statCb(err, stat) {
-
-        var type = typer(err, stat);
+        var type = self.typeset(err, stat, item) || typer(err, stat, item);
 
         //self.emit(type, {type: type, filepath: item, stat: stat, error: err});
         self.emit('*', {type: type, filepath: item, stat: stat, error: err});
 
+        /**
+         * @param {string} subitem File name from fs.readdir().
+         * @return {string} Filesystem path of subitem.
+         */
+        function getPath(subitem) {
+            return path.join(item, subitem);
+        }
+
+        /**
+         * @param {object} err
+         * @param {array} sublist File names contained in current item
+         */
+        function recurse(err, sublist) {
+            self.relatively(list.concat(sublist.map(getPath)));
+        }
+
         if ('dir' === type) {
-            fs.readdir(item, function (err, sublist) {
-                sublist.forEach(function (subitem) {
-                    list.push(path.join(item, subitem));
-                });
-                self.relatively(list);
-            });
+            fs.readdir(item, recurse);
         } else if (list.length) {
             self.relatively(list);
         } else {
@@ -40,6 +61,19 @@ function getStatCb(item, list, self) {
     };
 }
 
+/**
+ * @param {function} fn Function to allow custom event categories/types. The
+ * function will be passed err, stat, and pathname. If fn doesn't return a
+ * truthy value, the default typer will be used.
+ */
+function typeSetter(fn) {
+    this.typeset = 'function' === typeof fn ? fn : function() {};
+}
+
+/**
+ * @param {array} list Paths to begin walking. Events emitted for every item.
+ * Pathnames emitted are relative to the pathnames in the list.
+ */
 function relatively(list) {
     var item = list.shift();
     if (item) {
@@ -48,18 +82,23 @@ function relatively(list) {
     }
 }
 
+/**
+ * @param {array} list Paths to begin walking. Events emitted for every item.
+ * Pathnames emitted are absolute.
+ */
 function absolutely(list) {
-    return this.relative(list.map(path.resolve));
+    return this.relatively(list.map(path.resolve));
 }
 
 function Scan() {
     this.count = 0;
-    //this.typer = typerFn ? typerFn : typer;
+    this.typeSetter();
 }
 
 Scan.prototype = Object.create(Stream.prototype, {
     relatively: {value: relatively},
-    absolutely: {value: absolutely}
+    absolutely: {value: absolutely},
+    typeSetter: {value: typeSetter}
 });
 
 module.exports = Scan;
